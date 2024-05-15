0b73083301c0e101affbe6f354a6b564556b7a3a
No~More <81039083+N-o-M-o-r-e@users.noreply.github.com>
2024-05-15 15:51:00 +0700

    update title wallpaper

--- app/src/main/java/com/project/tathanhson/wallpaperandringtons/view/fragment/list/WallpaperFragment.kt
+++ app/src/main/java/com/project/tathanhson/wallpaperandringtons/view/fragment/list/WallpaperFragment.kt
@@ -20,14 +20,10 @@ import com.project.tathanhson.wallpaperandringtons.model.wallpaper.Title
 class WallpaperFragment : Fragment() {
     private lateinit var context: Context
     private lateinit var binding: FragmentWallpaperBinding
-
     lateinit var viewModel: WallpaperMV
     private var adapter: ListWallpaperAdapter? = null
     private var adapterTitle: TitleWallpaperAdapter? = null
     private var listWallpaper: WallpaperList? = null
-
-
-    var listTitle: ArrayList<Title> = ArrayList()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -40,49 +36,42 @@ class WallpaperFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentWallpaperBinding.inflate(inflater, container, false)
-        initViewModel()
-        crateData()
+        viewModel = ViewModelProvider(requireActivity())[WallpaperMV::class.java]
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity())[WallpaperMV::class.java]
+        crateData()
         intiView()
     }
 
     private fun crateData() {
-
-        viewModel.ldListTitle.observe(viewLifecycleOwner, Observer{ listTitle ->
-            listTitle?.let {
-                adapterTitle = TitleWallpaperAdapter(context, viewModel, viewLifecycleOwner, listTitle)
-                binding.rcvTitle.adapter = adapterTitle
-            }
-
-        })
-
-
-
-    }
-
-    private fun initViewModel() {
+        //call api để lấy dữ liệu title
         viewModel.getApiWallpaperTitle()
 
+        //sau đó call api với id của title trả về tương ứng
         viewModel.ldItemTitle.observe(viewLifecycleOwner, Observer{ titleSelected ->
             titleSelected?.id.let {
                 viewModel.getApiWallpaperDetail(titleSelected!!.id)
             }
         })
-
     }
 
-
     private fun intiView() {
+        //Quan sát dữ liệu khi call title category trả về để gán lên adapter
+        viewModel.ldListTitle.observe(viewLifecycleOwner, Observer{ listTitle ->
+            listTitle?.let {
+                adapterTitle = TitleWallpaperAdapter(context, viewModel, viewLifecycleOwner, listTitle)
+                binding.rcvTitle.adapter = adapterTitle
+            }
+        })
+
+        //Quan sát dữu liệu wallpaper sau khi call gán lên adapter
         viewModel.ldListWallpaper.observe(viewLifecycleOwner, Observer { wallpaperList ->
             wallpaperList?.let {
                 listWallpaper = wallpaperList
-                adapter =
-                    ListWallpaperAdapter(context, viewModel, viewLifecycleOwner, wallpaperList)
+                adapter = ListWallpaperAdapter(context, viewModel, viewLifecycleOwner, wallpaperList)
                 val layoutManager = GridLayoutManager(context, 3)
                 binding.rcvWallpaper.layoutManager = layoutManager
                 binding.rcvWallpaper.adapter = adapter
